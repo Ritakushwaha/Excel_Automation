4362c1b8782d25f2e2ee8854de630824d1fb91af
Rita Kushwaha <>
2021-03-18 01:14:19 +0530

    Effort_3_2021

--- Create_Excel.py
+++ Create_Excel.py
@@ -1,49 +1,44 @@
 # https://xlsxwriter.readthedocs.io/contents.html
 
 import xlsxwriter
-import datetime
-
+from datetime import datetime
 
 # Create a workbook and add a worksheet.
-var = str(datetime.datetime.today()).split()[0]
-workbook = xlsxwriter.Workbook(f'Efforts{var}.xlsx')
-worksheet = workbook.add_worksheet()
+month = datetime.today().month
+year = datetime.today().year
+dt = str(datetime.today()).split()[0]
+
+workbook = xlsxwriter.Workbook(f'Efforts_{month}_{year}.xlsx')
+worksheet = workbook.add_worksheet(f'data_{dt}.xlsx')
 
 # Add a bold format to use to highlight cells.
-bold = workbook.add_format({'bold' : True})
-
-# Add a number format for cells with money.
-money = workbook.add_format({'num_format' : '$#,##0'})
+cf = workbook.add_format({'bg_color': 'yellow'})
 
 # Write some data headers.
-worksheet.write('A1', 'Ticket No', bold)
-worksheet.write('B1', 'Ref No', bold)
-worksheet.write('C1', 'Corp ID', bold)
-worksheet.write('D1', 'Activity Type', bold)
-worksheet.write('E1', 'Activity', bold)
-worksheet.write('F1', 'Timecard Entry Date', bold)
-worksheet.write('G1', 'Effort', bold)
-worksheet.write('H1', 'Complexity', bold)
-worksheet.write('I1', 'AMorAD', bold)
-worksheet.write('J1', 'SOW', bold)
-worksheet.write('K1', 'Project', bold)
+worksheet.write('A1', 'Ticket No', cf)
+worksheet.write('B1', 'Ref No', cf)
+worksheet.write('C1', 'Corp ID', cf)
+worksheet.write('D1', 'Activity Type', cf)
+worksheet.write('E1', 'Activity', cf)
+worksheet.write('F1', 'Timecard Entry Date', cf)
+worksheet.write('G1', 'Effort', cf)
+worksheet.write('H1', 'Complexity', cf)
+worksheet.write('I1', 'AMorAD', cf)
+worksheet.write('J1', 'SOW', cf)
+worksheet.write('K1', 'Project', cf)
 
-tickets = ['INC123', 'INC234']
+tickets = ['','0','CHG0056957','CHG0056935','SCTASK0081896','INC0666400','SCTASK0083834','CHG0056957','INC0662672','INC0667274','INC0667610']
+l = len(tickets)-1
 corp_id = 'rikushwa'
 project = 'TOPSI'
 complexity = 'Medium'
-activity_type = 'Incident'
-activity = 'Incident'
-reference = ' '
-date = str(datetime.datetime.today()).split()[0]
-effort = 0.5
+activity_type = ''
+activity = ''
+reference = ''
+effort = 0
 AMorAD = ''
 SOW = ''
-
-# Some data we want to write to the worksheet.
-'''ticket_details = (
-    [tickets, reference, corp_id, activity_type, activity, date, effort, complexity, AMorAD, SOW, project],
-)'''
+date_format = workbook.add_format({'num_format':'yyyy-mm-dd hh:mm:ss'})
 
 # Start from the first cell below the headers.
 row = 1
@@ -51,14 +46,32 @@ col = 0
 
 # Iterate over the data and write it out row by row.
 for ticket in tickets:
-    ticket_details = ([ticket, reference, corp_id, activity_type, activity, date, effort, complexity, AMorAD, SOW, project],)
+    if ticket == '':
+        activity_type = 'Meetings / Communication'
+        activity = 'Mail Communication'
+        effort = 1.5
+    elif ticket == '0':
+        activity_type = 'Service-Task'
+        activity = 'DSTUM'
+        effort = 1.5
+    elif ticket[:3] == 'CHG':
+        activity_type = 'Change Request'
+        activity = 'Third party coordination'
+        effort = 1
+    else:
+        activity_type = 'Incident'
+        activity = 'Incident'
+        effort = 0.75
+
+    ticket_details = ([ticket, reference, corp_id, activity_type, activity, dt, effort, complexity, AMorAD, SOW, project],)
+
     for tickets, reference, corp_id, activity_type, activity, date, effort, complexity, AMorAD, SOW, project in ticket_details :
         worksheet.write(row, col, tickets)
         worksheet.write(row, col + 1, reference)
         worksheet.write(row, col + 2, corp_id)
         worksheet.write(row, col + 3, activity_type)
         worksheet.write(row, col + 4, activity)
-        worksheet.write(row, col + 5, date)
+        worksheet.write(row, col + 5, datetime.today(),date_format)
         worksheet.write(row, col + 6, effort)
         worksheet.write(row, col + 7, complexity)
         worksheet.write(row, col + 8, AMorAD)
@@ -66,4 +79,15 @@ for ticket in tickets:
         worksheet.write(row, col + 10, project)
         row += 1
 
-workbook.close()
+worksheet.write(row+1, col+6, f'=SUM(G3:G{l})')
+
+while True:
+    try:
+        workbook.close()
+    except xlsxwriter.exceptions.FileCreateError as e:
+        decision = input("Exception caught in workbook.close(): %s\n"
+                         "Please close the file if it is open in Excel.\n"
+                         "Try to write file again? Type [n]: " % e)
+        if decision != 'n':
+            continue
+    break
